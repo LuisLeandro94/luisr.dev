3c30613a5135a4d4fba8dcfd64f49d4f109cc00b
Luis Ribeiro <luisleandro.wd@gmail.com>
2024-07-30 02:58:22 +0100

    ui finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,17 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData(source, filePath) {
+            `@use "astro-breakpoints/use-breakpoints.scss" as *;`;
+          },
+        },
+      },
+    },
+  },
   integrations: [icon({
     include: {
       bxl: ["*"],
